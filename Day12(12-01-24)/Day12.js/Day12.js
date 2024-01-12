6511c84e924ec697f639d0063cb77ea826ded58b
Ritika Bhatia <139344947+iam-ritikabhatia@users.noreply.github.com>
2024-01-12 21:17:03 +0530

    Add files via upload

---
+++ Day12(12-01-24)/Day12.js/Day12.js
@@ -0,0 +1,18 @@
+// String ProtoTyping
+// What is String protoTyping ?
+// String ProtoTyping is To Modify the String in its own Way
+
+/************charAt***************/
+// It is used to find out the character at any index number
+
+// Syntax
+
+// console.log(string.charAt(index number))
+
+let myName = "Ramesh Sharma"
+console.log(myName.charAt(5)) // It returns the character at the index number 5
+
+let Name01 = "Ramesh Sharma"
+console.log(Name01[5]) // this will also return the character at index number, this will treat Name01 as an array and [5] means the character at index number 5
+
+/**a String is treated like an array in JavaScript and Python Language */
